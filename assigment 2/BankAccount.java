91df91a9555369a5a40da0e9ca6d182338d957f7
deepanshut041 <deepanshut041@gmail.com>
2017-10-08 14:05:18 +0530

    added Bankaccount class now working on testing

---
+++ assigment 2/BankAccount.java
@@ -0,0 +1,18 @@
+class BankAccount{
+    double balance;
+    String accountOwner;
+
+    public BankAccount(String owner){
+        balance = 27;
+        accountOwner = owner;
+    }
+
+    void deposit(double amount){
+        balance = balance + amount;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        return ((BankAccount) o).accountOwner).equals(accountOwner);
+    }
+}
