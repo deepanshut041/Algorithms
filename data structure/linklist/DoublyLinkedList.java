6e1fa70f89159019729a41c51383c5a682aee0d1
unknown <deepanshut041@gamil.com>
2017-12-26 19:02:42 +0530

    made doubly node now working on doubly linklist

--- data structure/linklist/DoublyLinkedList.java
+++ data structure/linklist/DoublyLinkedList.java
@@ -5,3 +5,33 @@ public class DoublyLinkedList {
 
     
 }
+class DoublyNode<T> implements Comparable<T> {
+
+    private T value;
+    private DoublyNode<T> nextRef;
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public DoublyNode<T> getNextRef() {
+        return nextRef;
+    }
+
+    public void setNextRef(DoublyNode<T> ref) {
+        this.nextRef = ref;
+    }
+
+    @Override
+    public int compareTo(T arg) {
+        if (arg == this.value) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+}
